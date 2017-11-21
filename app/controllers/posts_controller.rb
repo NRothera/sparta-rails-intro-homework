1c39747b3fb153f2f63547b00c7cd66e76ba7019
NRothera <31472463+NRothera@users.noreply.github.com>
2017-11-21 21:31:44 +0000

    can create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @post = Post.all
   end
 
   def show
@@ -14,4 +14,24 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+        end
+      end
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/edit'
+
+  get 'posts/new'
+
+  get 'posts/destroy'
+
   resources :posts
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
